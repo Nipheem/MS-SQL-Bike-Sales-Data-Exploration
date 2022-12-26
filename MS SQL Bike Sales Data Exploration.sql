c2f1dc6c26515e026bd9727b4ef14fb070ad34d3
Quadri Azeez <120409647+Nipheem@users.noreply.github.com>
2022-12-26 09:36:37 +0100

    Add files via upload

---
+++ MS SQL Bike Sales Data Exploration.sql
@@ -0,0 +1,35 @@
+SELECT
+	ord.order_id,
+	CONCAT(cus.first_name,' ',cus.last_name) AS customer_name,
+	cus.city,
+	cus.state,
+	ord.order_date,
+	SUM(itm.quantity) AS total_units,
+	SUM(itm.quantity* itm.list_price) AS revenue,
+	pro.product_name,
+	cat.category_name,
+	sto.store_name,
+	CONCAT(sta.first_name,' ',sta.last_name) AS sales_rep
+FROM sales.orders ord
+JOIN sales.customers cus
+ON ord.customer_id = cus.customer_id
+JOIN sales.order_items itm
+On ord.order_id = itm.order_id
+JOIN production.products pro
+ON pro.product_id = itm.product_id
+JOIN production.categories cat
+ON pro.category_id = cat.category_id
+JOIN sales.stores sto
+ON ord.store_id = sto.store_id
+JOIN sales.staffs sta
+ON sta.staff_id = ord.staff_id
+GROUP BY 
+ord.order_id,
+	CONCAT(cus.first_name,' ',cus.last_name),
+	cus.city,
+	cus.state,
+	ord.order_date,
+	pro.product_name,
+	cat.category_name,
+	sto.store_name,
+	CONCAT(sta.first_name,' ',sta.last_name)
